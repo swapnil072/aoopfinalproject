26f3a3d48da6b5a03aa570990ba8b2a8b6260c60
swapnil <sbiswas192072@bscse.uiu.ac.bd>
2021-01-31 02:15:42 +0600

    test2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,11 +14,12 @@ import java.io.*;
 import java.net.Socket;
 public class Controller {
     @FXML
-  public Button login,returnbook,searchbook,addbooks,issuebook,back;
+  public Button login,returnbook,searchbook,addbooks,issuebook,back,ic,ab,rb;
     @FXML
-    javafx.scene.control.TextField user,pass;
+    javafx.scene.control.TextField user,pass,name,id,idate,bookid,name1,id1,idate1,bookid1;
     BufferedReader reader = null;
     BufferedWriter writer = null;
+
     public Controller() {
         try {
             Socket socket = new Socket("127.0.0.1", 3005);
@@ -33,29 +34,33 @@ public class Controller {
     }
    @FXML
     public void loginClick() throws IOException {
-       Parent root = FXMLLoader.load(getClass().getResource("library.fxml"));
-       Stage window = (Stage) login.getScene().getWindow();
-       window.setScene(new Scene(root,600,575));
-
        String username = user.getText();
        String password = pass.getText();
        if (username.equals("") || password.equals("")) {
            JOptionPane.showMessageDialog(null, "Please fill up all the blanks");
        }
 
-//       try{
-//           writer.write("Login\n");
-//           writer.write(username);
-//           writer.newLine();
-//           writer.write(password);
-//           writer.newLine();
-//           writer.flush();
-//           String msg = reader.readLine();
-//           System.out.println(msg);
-//
-//       }catch (NumberFormatException | IOException e){
-//           e.printStackTrace();
-//       }
+
+       try{
+           writer.write("Login\n");
+           writer.write(username);
+           writer.newLine();
+           writer.write(password);
+           writer.newLine();
+           writer.flush();
+           String msg = reader.readLine();
+           System.out.println(msg);
+           if (msg.equals("Loginsuccessful")){
+               Parent root = FXMLLoader.load(getClass().getResource("library.fxml"));
+               Stage window = (Stage) login.getScene().getWindow();
+               window.setScene(new Scene(root,600,575));
+           }
+           else
+               JOptionPane.showMessageDialog(null, "Incorrect username and password");
+
+       }catch (NumberFormatException | IOException e){
+           e.printStackTrace();
+       }
    }
    @FXML
     public void addbooksClick() throws IOException {
@@ -63,15 +68,35 @@ public class Controller {
        Stage window = (Stage) addbooks.getScene().getWindow();
        window.setScene(new Scene(root,600,575));
 
-
-
-
    }
    @FXML public void issuebookclick() throws IOException {
        Parent root = FXMLLoader.load(getClass().getResource("issuebook.fxml"));
        Stage window = (Stage) issuebook.getScene().getWindow();
        window.setScene(new Scene(root,600,575));
+   }
+   @FXML
+   public void iclick(){
+       String n= name.getText();
+       String i =id.getText();
+       String date = idate.getText();
+       String book = bookid.getText();
+       try {
+           writer.write("IssueBook\n");
+           writer.write(n);
+           writer.newLine();
+           writer.write(i);
+           writer.newLine();
+           writer.write(date);
+           writer.newLine();
+           writer.write(book);
+           writer.newLine();
+           writer.flush();
+           String msg = reader.readLine();
 
+
+       }catch (IOException e){
+
+       }
 
    }
    @FXML
@@ -94,11 +119,16 @@ public class Controller {
        Stage window = (Stage) login.getScene().getWindow();
        window.setScene(new Scene(root,600,575));
 
+    }
 
-   }
+    @FXML
+    public void adbblick(){
 
+    }
+    @FXML
+    public void rbclick(){
 
-
+    }
 
    }
 
